48573c2ad97c6032c858cdfe32cf7bbde70722bf
Michael de Hoog <michael.dehoog@gmail.com>
2012-11-08 13:15:38 +0000

    Renamed user settings file to iscanduocp.settings

--- Settings/Constants.cs
+++ Settings/Constants.cs
@@ -9,9 +9,7 @@ namespace Profiler.Settings
     public class Constants
     {
         private readonly static string userDirectory = ParentDirectory(Application.LocalUserAppDataPath);
-        private readonly static string commonDirectory = ParentDirectory(Application.CommonAppDataPath);
-        private readonly static string userSettingsFile = userDirectory + Path.DirectorySeparatorChar + "user.settings";
-        private readonly static string commonSettingsFile = commonDirectory + Path.DirectorySeparatorChar + "common.settings";
+        private readonly static string userSettingsFile = userDirectory + Path.DirectorySeparatorChar + "iscanduocp.settings";
 
         private static string ParentDirectory(string path)
         {
@@ -21,11 +19,6 @@ namespace Profiler.Settings
         public static string UserSettingsFile
         {
             get { return userSettingsFile; }
-        }
-
-        public static string CommonSettingsFile
-        {
-            get { return commonSettingsFile; }
         }
     }
 }
